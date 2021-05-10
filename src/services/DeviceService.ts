8c75efd9bebb55432ed3e6fa3153292d19a2ed77
Enmanuel Carrasquel <enmanuelcarrasquel@ballastlane.com>
2021-05-10 16:03:19 -0400

    added disable internet feature

--- src/services/DeviceService.ts
+++ src/services/DeviceService.ts
@@ -1,14 +1,78 @@
 import { HttpService, Injectable } from '@nestjs/common';
-import { escape } from 'html-escaper';
-import { ConfigService } from '@nestjs/config';
 import LocalStore from '../utils/localStore';
-import { AuthService } from './AuthService';
-
+import Device from '../entities/Device';
 @Injectable()
 export class DeviceService {
-  constructor(
-    private httpService: HttpService,
-  ) {}
+  constructor(private httpService: HttpService) {}
+
+  addToHostList = (device: Device): Promise<any> => {
+    return this.httpService
+      .get(`/userRpm/AccessCtrlHostsListsRpm.htm`, {
+        params: {
+          addr_type: 0,
+          hosts_lists_name: device.name,
+          src_ip_start: '',
+          src_ip_end: '',
+          mac_addr: device.mac,
+          Changed: 0,
+          SelIndex: 0,
+          fromAdd: 0,
+          Page: 1,
+          Save: 'Guardar',
+        },
+      })
+      .toPromise();
+  };
+
+  addControlRuleForDevice = (device: Device): Promise<any> => {
+    return this.httpService
+      .get(`/userRpm/AccessCtrlAccessRulesRpm.htm`, {
+        params: {
+          rule_name: device.name,
+          hosts_lists: device.hostId,
+          targets_lists: 255,
+          scheds_lists: 255,
+          enable: device.enable ? 0 : 1,
+          Changed: 0,
+          SelIndex: 0,
+          Page: 1,
+          Save: 'Guardar',
+        },
+      })
+      .toPromise()
+      .then((res) => {
+        if (res.data.indexOf('errCode = "29006"') > 0) {
+          throw res.data;
+        }
+      });
+  };
+
+  changeSetup = async (mac: string, newDeviceSetup: Device): Promise<any> => {
+    const deviceList = await this.getDevices();
+    const deviceInfo = deviceList.find((u) => u.mac === mac);
+
+    if (deviceInfo.hostId < 0) {
+      await this.addToHostList(deviceInfo);
+      const hostId = deviceList.reduce(
+        (max, device) => (device.hostId > max ? device.hostId : max),
+        -1,
+      );
+      deviceInfo.hostId = hostId + 1;
+      await LocalStore.saveStoreData(deviceList);
+    }
+
+    if (
+      newDeviceSetup.enable !== undefined &&
+      newDeviceSetup.enable !== deviceInfo.enable
+    ) {
+      if (!newDeviceSetup.enable) {
+        await this.addControlRuleForDevice(deviceInfo);
+        deviceInfo.enable = newDeviceSetup.enable;
+      }
+    }
+
+    await LocalStore.saveStoreData(deviceList);
+  };
 
   getDevices = async (): Promise<any> => {
     const savedData = await LocalStore.getStoreData();
@@ -20,7 +84,7 @@ export class DeviceService {
     const userList = response.data.split('SCRIPT')[1];
 
     if (!userList) {
-      console.log('From backup')
+      console.log('From backup');
       return savedData;
     }
 
@@ -53,7 +117,16 @@ export class DeviceService {
 
     const [_, ...cleanUserList] = dirtyUsers;
 
-    await LocalStore.saveStoreData(cleanUserList);
-    return cleanUserList;
+    const withEnable = cleanUserList.map((user) => {
+      const userSaved = savedData.find((u) => u.mac === user.mac);
+      return {
+        ...user,
+        enable: userSaved ? userSaved.enable : true,
+        hostId: userSaved ? userSaved.hostId : -1,
+      };
+    });
+
+    await LocalStore.saveStoreData(withEnable);
+    return withEnable;
   };
 }
